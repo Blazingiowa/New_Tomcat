138c5c4b81c6fc5eb2690d43c51924408829620c
kamin <kamin@DESKTOP-7IMV15N>
2019-06-06 12:21:00 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/StartupText.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/StartupText.java
@@ -1,6 +1,13 @@
 package test_tomcat_git;
 
+import java.io.File;
+
 public class StartupText extends Text
 {
-	
+	DataBaseConnect DBC = new DataBaseConnect();
+
+	void createcardlist()
+	{
+		file = new File("");
+	}
 }
