0cc501fd0fb838bfcaad42e9d6dbc7bf918b10ce
kamin <kamin@DESKTOP-7IMV15N>
2019-06-20 14:10:12 +0900

    555

---
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/TextRead.java
@@ -0,0 +1,15 @@
+package test_tomcat_git;
+
+import java.io.BufferedReader;
+
+public class TextRead
+{
+	BufferedReader br;
+
+	int[] read()
+	{
+
+
+		return null;
+	}
+}
---
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/TextWriter.java
@@ -0,0 +1,17 @@
+package test_tomcat_git;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+
+public class TextWriter
+{
+	BufferedReader br;
+	BufferedWriter bw;
+	PrintWriter pw;
+
+	void write()
+	{
+
+	}
+}
