bfa3cdadaa16a55469ab03a0c419f85d60a96f18
小林　史昌 <@192.168.10.11>
2019-06-21 22:28:13 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/StartupText.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/StartupText.java
@@ -1,15 +1,33 @@
 package test_tomcat_git;
 
-import java.io.File;
-
 public class StartupText extends TextWrite
 {
-
 	DataBaseConnectRead DBCR = new DataBaseConnectRead();
 
-
-	void createcardlist()
+	void textfile(int room_id,int player_number,int line_number)
 	{
-		file = new File("");
+		settext(room_id,player_number);
+		text = ""; writetext = "";
+
+		int[] defSet = new int[3];
+		int[] hpSet = {-1,100,100};
+		int[] manaSet = {-1,1,1};
+		int[] defDamage = {0,0,0};
+
+		for(int i = 0;i<defSet.length;i++)
+		{
+			defSet[i] = -1;
+		}
+
+		for(int i = 0;i<defSet.length;i++)//linestrに1行分の-1を挿入
+		{
+			writetext += defSet[i];
+
+			if((i+1)<defSet.length)
+			{
+				writetext += ",";
+			}
+		}
+
 	}
 }
