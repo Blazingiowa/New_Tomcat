9293e3fe83e8121e0c5e67ee5c27921a181c21ff
小林　史昌 <@192.168.10.11>
2019-06-21 22:20:51 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/TextWriter.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/TextWriter.java
@@ -1,15 +1,72 @@
 package test_tomcat_git;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class TextWriter extends TextRead
 {
 	BufferedWriter bw;
 	PrintWriter pw;
+	FileWriter fw;
+	String text,writetext;
 
 	void write(int room_id,int player_number,int line_number,int[] write)
 	{
 		settext(room_id,player_number);
+		text = "" ; writetext = "";
+
+		for(int i =0;i<write.length;i++)
+		{
+			writetext += write[i];
+			if((i+1)<write.length)
+			{
+				writetext += ",";
+			}
+		}
+
+		line[line_number] = writetext;
+
+		for(int i = 0;i<line.length;i++)
+		{
+			text += line[i];
+
+			if((i+1)<line.length)
+			{
+				text += "s";
+			}
+		}
+
+		try
+		{
+			fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+			pw = new PrintWriter(bw);
+			pw.println(text);
+		}
+		catch(Exception e)
+		{
+
+		}
+		finally
+		{
+			bwclose();
+		}
+	}
+
+	void bwclose()
+	{
+		if(bw != null)
+		{
+			try
+			{
+				bw.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 }
