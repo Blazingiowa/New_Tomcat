ec42b729a7c6dcdbe1addca3492b1f3cbd89b5e1
tukiya_kanna <kaminashitukiya@gmail.com>
2019-06-26 15:22:00 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/Gamestart.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/Gamestart.java
@@ -26,7 +26,7 @@ public class Gamestart
 			userinfo[i] = String.valueOf(player[i]);
 		}
 
-		file = new File("var/www/html/"+player[1]+"/"+player[2]+".txt");
+		file = new File("/var/www/html/"+player[1]+"/"+player[2]+".txt");
 		if(file.exists() == false)//player.txt
 		{
 			createfile(file);
@@ -34,7 +34,7 @@ public class Gamestart
 
 		tx.editer(player[1], player[2],0,0,null);
 
-		file = new File("var/www/html/"+player[1]+"/taiou.txt");
+		file = new File("/var/www/html/"+player[1]+"/taiou.txt");
 
 		if(file.exists() == false)//対応表の有無
 		{
@@ -42,7 +42,7 @@ public class Gamestart
 			tt.taioucreate(player[1]);
 		}
 
-		file = new File("var/www/html/"+player[1]+"/card.txt");
+		file = new File("/var/www/html/"+player[1]+"/card.txt");
 
 		if(file.exists() == false)//カード表の有無
 		{
@@ -50,15 +50,15 @@ public class Gamestart
 			ct.cardcreate(player[1]);
 		}
 
-		file = new File("var/www/html/"+player[1]+"/room.txt");
+		file = new File("/var/www/html/"+player[1]+"/room.txt");
 
 		if(file.exists() == false)//room.txtの有無
 		{
 			createfile(file);
 			rt.createroomtxt(player[1],file);
 		}
-		
-		
+
+
 
 
 
