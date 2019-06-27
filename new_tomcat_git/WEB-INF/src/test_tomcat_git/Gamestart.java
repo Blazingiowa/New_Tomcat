8176cb502f9c5c0fb2424aa503fcf1040ca02c37
tukiya_kanna <kaminashitukiya@gmail.com>
2019-06-27 15:47:53 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/Gamestart.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/Gamestart.java
@@ -83,6 +83,9 @@ public class Gamestart
 			if(newfile.createNewFile())
 			{
 				System.out.println("ファイル作れたお");
+				newfile.setWritable(true,true);
+				newfile.setReadable(true);
+				newfile.setExecutable(true);
 			}
 		}
 
