6192231fb1f13d898b44465d1859e19008db7da8
tukiya_kanna <kaminashitukiya@gmail.com>
2019-07-01 14:07:38 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/TextRead.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/TextRead.java
@@ -18,10 +18,7 @@ public class TextRead
 	int[] read(int room_id,int player_number,int line_number)
 	{
 		line = settext(room_id,player_number);
-		for(int i = 0;i<line.length;i++)
-		{
-			System.out.println(line[i]);
-		}
+
 
 		String[] array = line[line_number].split(",");
 
@@ -96,6 +93,10 @@ public class TextRead
 			brclose();
 		}
 
+		for(int i = 0;i<line.length;i++)
+		{
+			System.out.println(line[i]);
+		}
 		return line;
 	}
 
