c6b9a900b08565b2347ef4e727f2d86c355e599c
tukiya_kanna <kaminashitukiya@gmail.com>
2019-07-03 15:39:38 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/TextRead.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/TextRead.java
@@ -2,6 +2,8 @@ package test_tomcat_git;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TextRead
 {
@@ -10,17 +12,16 @@ public class TextRead
 	String text;
 	String[][] alltext;
 	String[] line;
-	SetText set = new SetText();
+	//SetText set = new SetText();
 
 	int[] data;
 
 	int[] read(int room_id,int player_number,int line_number)
 	{
-		line = set.settext(room_id,player_number);
+		line = settext(room_id,player_number);
 
 
 		String[] array = line[line_number].split(",");
-		data = new int[array.length];
 
 		for(int i = 0;i<array.length;i++)
 		{
@@ -28,6 +29,89 @@ public class TextRead
 		}
 
 		return data;
+	}
+
+	String[] settext(int room,int player)
+	{
+		if(player==3)
+		{
+			alltext = new String[2][3];
+			line = new String[2];
+			data = new int[3];
+			file = new File("/var/www/html/"+ room + "/room.txt");
+			System.out.println("room.txt見ようとしてるよ");
+		}
+		else
+		{
+			alltext = new String[7][3];
+			line = new String[7];
+			data = new int[3];
+			file = new File("/var/www/html/"+ room + "/"+player+".txt");
+		}
+
+
+
+		try
+		{
+			br = new BufferedReader(new FileReader(file));
+
+			text = br.readLine();
+			System.out.println("Readでのテキストファイルの中身");
+			System.out.println(text);
+
+			String[] array = text.split(",");
+
+			for(int i = 0,j = 0,k=0;k<array.length;k++)
+			{
+				if(!array[k].equals("s"))
+				{
+					alltext[i][j] = array[k];
+
+					j++;
+				}
+				else
+				{
+					i++;
+					j = 0;
+				}
+			}
+
+			for(int i = 0;i<alltext.length;i++)
+			{
+				line[i] = alltext[i][0]+","+alltext[i][1]+","+alltext[i][2];
+			}
+
+		}
+		catch(Exception e)
+		{
+
+		}
+		finally
+		{
+			brclose();
+		}
+
+		for(int i = 0;i<line.length;i++)
+		{
+			System.out.println(line[i]);
+		}
+		return line;
+	}
+
+	private void brclose()
+	{
+		if(br!=null)
+		{
+			try
+			{
+				br.close();
+			}
+
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
