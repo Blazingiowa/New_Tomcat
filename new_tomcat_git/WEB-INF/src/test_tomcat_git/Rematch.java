663af0682b33b7455cbd16bd0026a2928e3dc598
tukiya_kanna <kaminashitukiya@gmail.com>
2019-08-23 16:14:49 +0900

    555

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/Rematch.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/Rematch.java
@@ -2,7 +2,7 @@ package test_tomcat_git;
 
 import java.io.File;
 
-public class Rematch extends Gamestart
+public class Rematch extends Gamestart //リマッチ用のクラスunityからリマッチ要望が来た場合ここに飛ばしてね
 {
 	void resturt(int room_id)
 	{
@@ -12,7 +12,7 @@ public class Rematch extends Gamestart
 		files[2] = new File("/var/www/html/game/"+room_id+"/cooltime.txt");
 
 		st.textfile(room_id, 1, files[0]);
-		st.textfile(room_id, 1, files[1]);
+		st.textfile(room_id, 2, files[1]);
 
 		coolt.createcooltime(files[2]);
 
