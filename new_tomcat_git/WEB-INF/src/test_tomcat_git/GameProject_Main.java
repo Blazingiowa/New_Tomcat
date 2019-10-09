17efdb805d1632c950396f6875ffd15c9a2bbd9d
user <user@NCC-148737276>
2019-10-09 15:04:57 +0900

    final intを追記。固定数を排除？

--- new_tomcat_git/WEB-INF/src/test_tomcat_git/GameProject_Main.java
+++ new_tomcat_git/WEB-INF/src/test_tomcat_git/GameProject_Main.java
@@ -2,6 +2,30 @@ package test_tomcat_git;
 
 public class GameProject_Main
 {
+	final int Users = 2;//プレイヤー人数
+
+	final int UserInfo = 4;//プレイヤー情報
+
+	final int RoomInfo = 3;//ルーム状況
+
+	final int MaxUseCard = 3;//使えるカード枚数の限度
+
+	final int CardInfo = 9;//カードの情報（IDやCT、ダメージ、対応ID、カード種別）
+
+	final int WithHold = 3;//配列内を退避させるため
+
+	final int TextField = 7;//プレイヤーテキストの行
+
+	final int JudgePlayer1 = 1; //プレイヤー１かどうかの判定
+
+	final int JudgePlayer2 = 2;//プレイヤー２かどうかの判定
+
+	final int JudgeATKCard = 0;//攻撃カードかどうかの判定
+
+	final int JudgeCard = -1;//カードの有無・対応についての判定
+
+	final int[] JudgePlayering =
+	{ 0, 1, 2 };//ゲームの進行度を判定
 
 	//DBクラスのインスタンス
 	DataBaseConnectRead DBC = new DataBaseConnectRead();
@@ -14,7 +38,6 @@ public class GameProject_Main
 
 	//カードテキストクラスのインスタンス
 	CardText cardtext = new CardText();
-
 
 	int[] player;//ルーム・プレイヤーの状況を判定するための配列
 	int[][] p1_card;//統合処理の時の、カード判定時に使う
@@ -29,51 +52,48 @@ public class GameProject_Main
 	int[] textW;//テキストファイルの内容を一時的に避難させるための１次元配列
 	int[] textF;//テキストファイルの内容を一時的に避難させるための可変可能な１次元配列
 	int w;//一時退避変数
+	int MaxCardNum;//カードの枚数を格納するための変数
 	int[][] textmain;//避難させた内容を格納するための配列
 
 	//攻撃が通せるかどうか判定するための変数とフラグ
-	int count;
-	boolean flag;
+	int ATKcount;
+	boolean ATKflag;
 
 	//ソート前のカード情報を格納するための配列
-	int[][]bfrcard;
+	int[][] bfrcard;
 
-	int[][]CT;//クールタイムの情報を格納するための配列
+	int[][] CT;//クールタイムの情報を格納するための配列
 
 	//クールタイムの更新した情報をテキストに書き込む時に使う１次元配列
-	int[]CTwrite;
-
+	int[] CTwrite;
 
 	//コンストラクタ
 	GameProject_Main()
 	{
-		player = new int[3];
-		p1_card = new int[3][8];
-		p2_card = new int[3][8];
+		player = new int[RoomInfo];
+		p1_card = new int[MaxUseCard][CardInfo];
+		p2_card = new int[MaxUseCard][CardInfo];
 
+		textW = new int[WithHold];
+		textmain = new int[TextField][MaxUseCard];
 
-		textW = new int[3];
-		textmain = new int[7][3];
+		ATKcount = 0;
+		ATKflag = false;
 
-		count = 0;
-		flag = false;
-
-		bfrcard = new int[4][3];
+		bfrcard = new int[UserInfo][MaxUseCard];
 		/*---bfrcardの配列概要---*/
-		/*０行目：統合処理を行っているプレイヤーの使った順カードID*/
-		/*１行目：統合処理を行っているプレイヤーの発生したダメージを使った順に並び替え格納*/
-		/*２行目：統合処理に入っていないプレイヤーの使った順カードID*/
-		/*３行目：統合処理に入っていないプレイヤーの発生したダメージを使った順に並び替え格納*/
+		/*	０行目：統合処理を行っているプレイヤーの使った順カードID
+			１行目：統合処理を行っているプレイヤーの発生したダメージを使った順に並び替え格納
+			２行目：統合処理に入っていないプレイヤーの使った順カードID
+			３行目：統合処理に入っていないプレイヤーの発生したダメージを使った順に並び替え格納	*/
 
-
-		w = cardtext.CardCount();//DBに登録されているカード枚数を取得
+		MaxCardNum = cardtext.CardCount();//DBに登録されているカード枚数を取得
 
 		//カード枚数に応じて配列を作成
-		CT = new int[2][w];
-		CTwrite = new int[w];
+		CT = new int[Users][MaxCardNum];
+		CTwrite = new int[MaxCardNum];
 
 	}
-
 
 	/*infoの配列内容------------------------/useの配列の内容--------/
 	/										/						/
@@ -108,9 +128,10 @@ public class GameProject_Main
 		player = txR.read(info[1], 3, 1);
 
 		//それぞれのプレイヤーが処理が終わっているかどうかの判定
-		if (player[1] == 1 && player[2] == 1)
+		if (player[1] == JudgePlayering[1] && player[2] == JudgePlayering[1])
 		{
-			if (info[2] == 1)
+			//プレイヤー１
+			if (info[2] == JudgePlayer1)
 			{
 				//ｐ１で入った場合の統合処理
 				GPIP1.IntegratedP1(info);
@@ -118,7 +139,9 @@ public class GameProject_Main
 				//クールタイム処理
 				GPIP1.setCT(info);
 			}
-			else if (info[2] == 2)
+
+			//プレイヤー２
+			else if (info[2] == JudgePlayer2)
 			{
 				//ｐ２で入った場合の統合処理
 				GPIP2.IntegratedP2(info);
@@ -129,6 +152,5 @@ public class GameProject_Main
 
 		}
 	}
-
 
 }
